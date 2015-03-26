febb3e822dedc24bc62203acd3de23bab13340ca
AlexMl <alexandermlinzk@gmail.com>
2015-03-26 16:33:16 +0100

    Added textArea and set locations and size relative to each other

--- src/me/Alex/TSChat/Client/GUI/TSChatGUI.java
+++ src/me/Alex/TSChat/Client/GUI/TSChatGUI.java
@@ -5,17 +5,19 @@ import java.awt.EventQueue;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JList;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.JTextPane;
 
 
 public class TSChatGUI {
     
     private JFrame frame;
     private JTextField textField;
-    
+    private JTextArea textArea;
     private JButton btnSendButton;
+    
+    private static final int margin = 15;
+    private static final int buttonHeight = 40;
     
     /**
      * Launch the application.
@@ -48,34 +50,37 @@ public class TSChatGUI {
     private void initialize() {
 	this.frame = new JFrame();
 	this.frame.setBounds(100, 100, 600, 500);
-	this.frame.setMinimumSize(new Dimension(600, 400));
+	this.frame.setMinimumSize(new Dimension(600, 500));
 	this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	this.frame.getContentPane().setLayout(null);
 	
 	this.btnSendButton = new JButton("Senden");
-	this.btnSendButton.setSize(100, 50);
-	this.btnSendButton.setLocation((this.frame.getHeight() - (this.btnSendButton.getHeight() + 0)), this.frame.getWidth() - (this.btnSendButton.getWidth() + 10));
+	this.btnSendButton.setSize(100, buttonHeight);
+	this.btnSendButton.setLocation((this.frame.getWidth() - (this.btnSendButton.getWidth() + margin)), (this.frame.getHeight() - (this.btnSendButton.getHeight() + margin)));
 	this.frame.getContentPane().add(this.btnSendButton);
 	
 	this.textField = new JTextField();
-	this.textField.setBounds(24, 206, 290, 37);
+	this.textField.setSize(this.frame.getWidth() - (this.btnSendButton.getWidth() + 3 * margin), buttonHeight);
+	this.textField.setLocation(margin, (this.frame.getHeight() - (this.textField.getHeight() + margin)));
 	this.frame.getContentPane().add(this.textField);
-	this.textField.setColumns(10);
 	
-	JTextPane textPane = new JTextPane();
-	textPane.setBounds(31, 31, 283, 165);
-	this.frame.getContentPane().add(textPane);
+	/*	JList<String> list = new JList<String>();
+		list.setBounds(480, 89, 70, 146);
+		this.frame.getContentPane().add(list);
+		*/
 	
-	JList<String> list = new JList<String>();
-	list.setBounds(347, 31, 70, 146);
-	this.frame.getContentPane().add(list);
+	this.textArea = new JTextArea();
+	this.textArea.setEditable(false);
+	this.textArea.setSize(this.frame.getWidth() - 2 * margin, this.frame.getHeight() - (buttonHeight + 3 * margin));
+	this.textArea.setLocation(margin, margin);
+	this.frame.getContentPane().add(this.textArea);
+	
 	System.out.println(this.btnSendButton.getLocation());
 	System.out.println(this.frame.getBounds());
     }
     
     private void resize() {
 	
-	this.btnSendButton.setBounds(this.frame.getHeight() - (this.btnSendButton.getHeight() + 20), this.frame.getWidth() - (this.btnSendButton.getWidth() + 20), 100, 50);
-	
+	this.btnSendButton.setLocation((this.frame.getWidth() - (this.btnSendButton.getWidth() + margin)), (this.frame.getHeight() - (this.btnSendButton.getHeight() + margin)));
     }
 }
